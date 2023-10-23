ca461aecee84374a7afff6c902ae05d0905fd823
Khaled6120 <khalednaes3@gmail.com>
2023-10-23 19:12:27 +0300

    Add input validation + finalize the component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,24 +3,23 @@ import styles from "./input.module.css";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { useState } from "react";
 
-Input.propTypes = {
-  labelName: PropTypes.string.isRequired,
-  placeholder: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
-};
-
-function Input({ labelName, placeholder, type }) {
+const Input = ({ labelName, placeholder, type, onChange, errors, extra }) => {
   const [showPassword, setShowPassword] = useState(false);
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
+  const inputStyle = errors && errors[type] ? { borderColor: "#ee4331" } : {};
+
   return (
     <div className={styles.container}>
-      <label>{labelName}</label>
+      <div className={styles.extra_container}>
+        <label>{labelName}</label>
+        {extra && <a>{extra}</a>}
+      </div>
       <div className={styles.inputContainer}>
-        <input type={showPassword ? "text" : type} placeholder={placeholder} />
+        <input onChange={onChange} type={showPassword ? "text" : type} placeholder={placeholder} style={inputStyle} />
         {type === "password" && (
           <div className={styles.iconContainer} onClick={togglePasswordVisibility}>
             {showPassword ? <FaEyeSlash /> : <FaEye />}
@@ -29,6 +28,16 @@ function Input({ labelName, placeholder, type }) {
       </div>
     </div>
   );
-}
+};
+
+Input.propTypes = {
+  labelName: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  onChange: PropTypes.func,
+  errors: PropTypes.object,
+  value: PropTypes.string,
+  extra: PropTypes.string,
+};
 
 export default Input;

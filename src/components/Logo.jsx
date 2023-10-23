efa0b0f41a8cac059e01920f0cac2f473a13721a
Khaled6120 <khalednaes3@gmail.com>
2023-10-23 19:14:36 +0300

    update logo component

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import logo from "../../public/logo_1.png";
+import styles from "./logo.module.css";
+
+const Logo = () => {
+  return (
+    <div className={styles.container}>
+      <img src={logo} alt="Logo" width="120px" height="48px" />
+    </div>
+  );
+};
+
+export default Logo;
